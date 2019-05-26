affcbf89d46380eb848f961a3aaef9b3167a657e
krrathore1990 <karanr15.rathore@gmail.com>
2019-05-26 22:16:24 +0530

    first commit

---
+++ src/main/java/com/ark/arkbackendauthservice/ArkBackendAuthserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ark.arkbackendauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkBackendAuthserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkBackendAuthserviceApplication.class, args);
+	}
+
+}
